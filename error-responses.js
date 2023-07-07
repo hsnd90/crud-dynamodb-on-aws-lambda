909f9b240b2f96aaed45e6ac10ef375fa56f126e
Hasan <44682484+hsnd90@users.noreply.github.com>
2023-07-07 04:07:40 +0300

    some fixes.

--- error-responses.js
+++ error-responses.js
@@ -1,16 +1,22 @@
 const MISSING_TABLENAME = {
   statusCode: 400,
-  errorMessage: 'Missing TableName',
+  body: {
+    errorMessage: 'Missing TableName',
+  },
 };
 
 const MISSING_ID = {
   statusCode: 400,
-  errorMessage: 'Missing id',
+  body: {
+    errorMessage: 'Missing id',
+  },
 };
 
 const MISSING_BODY = {
   statusCode: 400,
-  errorMessage: 'Missing data',
+  body: {
+    errorMessage: 'Missing data',
+  },
 };
 
 export { MISSING_BODY, MISSING_ID, MISSING_TABLENAME };
